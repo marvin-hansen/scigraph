f9b930c178329ed96a34277af8120e0327cf807d
Marvin Hansen <marvin.hansen@gmail.com>
2022-02-23 11:28:37 +0800

    small fix

--- scripts/db/local/configure-local-db.sh
+++ scripts/db/local/configure-local-db.sh
@@ -1,8 +1,6 @@
-#
+#!/bin/bash
 # Copyright (c) 2021. Marvin Friedrich Lars Hansen. All Rights Reserved. Contact: marvin.hansen@gmail.com
-#
 
-# bin/bash
 set -o errexit
 set -o nounset
 set -o pipefail
