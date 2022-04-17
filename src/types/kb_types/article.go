11c6dac2fc6e26b210a54c84dd265e88f919f59d
Marvin Hansen <marvin.hansen@gmail.com>
2022-04-17 12:46:50 +0800

    Removed primary category b/c of its redundancy.

--- src/types/kb_types/article.go
+++ src/types/kb_types/article.go
@@ -6,24 +6,24 @@ import (
 )
 
 type Publication struct {
-	GUID            uint64      `json:"guid"`
-	ID              string      `json:"id"`
-	Doi             string      `json:"doi,omitempty"`
-	Title           string      `json:"title"`
-	Published       string      `json:"published"`
-	Updated         string      `json:"updated"`
-	Comment         string      `json:"comment"`
-	Summary         string      `json:"summary"`
-	Author          []*Author   `json:"author"`
-	Link            []*Link     `json:"link,omitempty"`
-	PrimaryCategory *Category   `json:"primaryCategory,omitempty"`
-	Category        []*Category `json:"category,omitempty"`
-	Concept         []*Concept  `json:"concept,omitempty"`
+	GUID      uint64    `json:"guid"`
+	ID        string    `json:"id"`
+	Doi       string    `json:"doi,omitempty"`
+	Title     string    `json:"title"`
+	Published string    `json:"published"`
+	Updated   string    `json:"updated"`
+	Comment   string    `json:"comment"`
+	Summary   string    `json:"summary"`
+	Author    []*Author `json:"author"`
+	Link      []*Link   `json:"link,omitempty"`
+	//PrimaryCategory *Category   `json:"primaryCategory,omitempty"`
+	Category []*Category `json:"category,omitempty"`
+	Concept  []*Concept  `json:"concept,omitempty"`
 }
 
 func (s Publication) String() string {
-	return fmt.Sprintf("Publication: \n GUID %v \n ID: %v \n DOI: %v \n Title: %v \n Published : %v \n Updated: %v \n Comment: %v \n Authord: %v \n  Link: %v \n PrimaryCategory: %v \n Category: %v \n   Concept: %v \n Summary: %v \n",
-		s.GUID, s.ID, s.Doi, s.Title, s.Published, s.Updated, s.Comment, s.Author, s.Link, s.PrimaryCategory,
+	return fmt.Sprintf("Publication: \n GUID %v \n ID: %v \n DOI: %v \n Title: %v \n Published : %v \n Updated: %v \n Comment: %v \n Authord: %v \n  Link: %v \n Category: %v \n   Concept: %v \n Summary: %v \n",
+		s.GUID, s.ID, s.Doi, s.Title, s.Published, s.Updated, s.Comment, s.Author, s.Link,
 		s.Category, s.Concept, s.Summary)
 }
 
