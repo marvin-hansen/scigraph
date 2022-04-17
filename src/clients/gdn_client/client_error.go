38b6282db8a37577b02a20dba4d4a32865059d67
Marvin Hansen <marvin.hansen@gmail.com>
2022-04-17 14:29:35 +0800

    Updated API error to match server response

--- src/clients/gdn_client/client_error.go
+++ src/clients/gdn_client/client_error.go
@@ -6,11 +6,13 @@ import (
 
 // APIError return the api error
 type APIError struct {
-	Status  int
-	Message string
+	Code         int    `json:"code"`
+	IsError      bool   `json:"error"`
+	ErrorMessage string `json:"errorMessage"`
+	ErrorNum     int    `json:"errorNum"`
 }
 
 // Error return the error message
 func (e APIError) Error() string {
-	return fmt.Sprintf("APIError: status=%d, message=%s", e.Status, e.Message)
+	return fmt.Sprintf("APIError: Code=%v, Number=%v,  Error Message=%v", e.Code, e.ErrorNum, e.ErrorMessage)
 }
