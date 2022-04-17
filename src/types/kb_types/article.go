47161aaaab75c7f0130ece42c7b96adb739f411f
Marvin Hansen <marvin.hansen@gmail.com>
2022-04-17 12:03:18 +0800

    Added Hash & GetGuidString methods.

--- src/types/kb_types/article.go
+++ src/types/kb_types/article.go
@@ -1,18 +1,21 @@
 package kb_types
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+)
 
 type Publication struct {
 	GUID            uint64      `json:"guid"`
 	ID              string      `json:"id"`
-	Doi             string      `json:"doi"`
+	Doi             string      `json:"doi,omitempty"`
 	Title           string      `json:"title"`
 	Published       string      `json:"published"`
 	Updated         string      `json:"updated"`
 	Comment         string      `json:"comment"`
 	Summary         string      `json:"summary"`
 	Author          []*Author   `json:"author"`
-	Link            []*Link     `json:"link"`
+	Link            []*Link     `json:"link,omitempty"`
 	PrimaryCategory *Category   `json:"primaryCategory,omitempty"`
 	Category        []*Category `json:"category,omitempty"`
 	Concept         []*Concept  `json:"concept,omitempty"`
@@ -24,6 +27,14 @@ func (s Publication) String() string {
 		s.Category, s.Concept, s.Summary)
 }
 
+func (s Publication) GetGuidString() string {
+	return strconv.FormatUint(s.GUID, 10)
+}
+
 func (s Publication) GetGuid() uint64 {
+	return s.GUID
+}
+
+func (s Publication) Hash() uint64 {
 	return s.GUID
 }
